303f4766e1192264c276c49fad2e1de91d4d2d6e
ok2ju <soulSpringg@gmail.com>
2015-12-07 19:11:05 +0300

    list of companies

--- app/js/vacancy/list/index.js
+++ app/js/vacancy/list/index.js
@@ -1,6 +1,7 @@
 require('angular')
   .module('app.vacancy.list', [
-    'app.resource.vacancy'
+    'app.resource.vacancy',
+    'app.filter.date'
   ])
     .config(require('./vacancyList.config'))
     .controller('VacancyListController', require('./vacancyList.controller'));
