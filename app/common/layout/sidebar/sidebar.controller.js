ffe43bcc73f92e5fc18404be72adedaa4d239b63
ok2ju <soulSpringg@gmail.com>
2016-01-19 19:12:46 +0300

    sidebar active menu

--- app/common/layout/sidebar/sidebar.controller.js
+++ app/common/layout/sidebar/sidebar.controller.js
@@ -3,51 +3,33 @@ module.exports = function LayoutSidebarController() {
 
     vm.items = [{
         name: 'Video Room',
-        url: '/videoroom',
-        active: false,
         icon: 'icon-videocam',
         state: 'app.videoroom'
     }, {
         name: 'Vacancies',
-        url: '/vancancies',
-        active: false,
         icon: 'icon-newspaper',
         state: 'app.vacanciesList'
     }, {
         name: 'Calendar',
-        url: '/calendar',
-        active: false,
         icon: 'icon-calendar',
         state: 'app.test'
     }, {
         name: 'Companies',
-        url: '/companies',
-        active: false,
         icon: 'icon-building',
         state: 'app.companies'
     }];
 
     vm.company = [{
         name: 'My Company',
-        url: '/company/manage',
-        active: false,
         icon: 'icon-diamond',
         state: 'app.manageCompany'
     }, {
         name: 'Candidates',
-        url: '/candidates',
-        active: true,
         icon: 'icon-users',
         state: 'app.test'
     }, {
         name: 'Vacancy',
-        url: '/vacancies/manage/companies',
-        active: false,
         icon: 'icon-newspaper',
         state: 'app.vacanciesManage.companies'
     }];
-
-    vm.btnToggler = function(menuItem) {
-        menuItem.active = true;
-    };
 };
