1589836fca6f20440888e5ab6d95e254fb599061
Ab0lt <andrei.baltrukevich@instinctools.ru>
2016-01-26 12:09:45 +0300

    fix vacancy creating dropdowns

--- app/src/components/vacancy/create/create.controller.js
+++ app/src/components/vacancy/create/create.controller.js
@@ -6,18 +6,15 @@ module.exports = function VacancyCreateController($state, VacancyResource,
   vm.registerVacancy = registerVacancy;
 
   // Fetch data for positions dropdown
-  vm.position = {};
   vm.positions = positions.data;
 
   // Fetch data for vacancy types dropdown
-  vm.vacancyType = {};
   vm.vacancyTypes = vacancyTypes.data;
 
   vm.company = {};
   vm.companies = companies;
 
   function registerVacancy() {
-    vm.vacancy.company_id = vm.company.selected._id;
     VacancyResource.postVacancy(vm.vacancy).then(function() {
       toastr.success('Vacancy created.', 'Yay!');
       $state.go('app.vacancy');
