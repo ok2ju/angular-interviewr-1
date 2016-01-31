d1e30b7c968dde21dc2d2ecad3201e160f483378
ok2ju <soulSpringg@gmail.com>
2016-01-31 14:46:35 +0300

    vacancy profile ui

--- app/src/components/vacancy/profile/vacancyProfile.controller.js
+++ app/src/components/vacancy/profile/vacancyProfile.controller.js
@@ -1,7 +1,15 @@
 module.exports = function VacancyProfileController(vacancyResource, $stateParams) {
   var vm = this;
 
+  vm.subscribe = subscribe;
+
   vacancyResource.oneVacancy($stateParams.id).then(function(vacancy) {
     vm.vacancy = vacancy;
   });
+
+  function subscribe(vacancy) {
+    vacancyResource.subscribe(vacancy).then(function() {
+      console.log('success subscruption!');
+    });
+  }
 };
