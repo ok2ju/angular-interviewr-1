83f3ba784ee8e78c2f49de6b77eaaadd6dd2b231
Aleh Atsman <aleh_atsman@epam.com>
2016-02-02 22:38:06 +0300

    added css for ui calendar.

--- app/src/components/calendar/calendar.controller.js
+++ app/src/components/calendar/calendar.controller.js
@@ -1,7 +1,20 @@
 module.exports = function CalendarController() {
   var vm = this;
 
-  vm.eventSources = [];
+  var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+  vm.events = [
+    {title: 'All Day Event',start: new Date(y, m, 1), editable: false},
+    {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+    {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+    {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+    {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+  ];
+
+  vm.eventSources = [vm.events];
 
   vm.uiConfig = {
     calendar:{
