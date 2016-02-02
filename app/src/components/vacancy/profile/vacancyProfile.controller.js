ca4b89e99583ae296fe38fb8df24483372290d4c
Ab0lt <andrei.baltrukevich@instinctools.ru>
2016-02-02 14:44:11 +0300

    list of vacancy followers

--- app/src/components/vacancy/profile/vacancyProfile.controller.js
+++ app/src/components/vacancy/profile/vacancyProfile.controller.js
@@ -1,15 +1,23 @@
-module.exports = function VacancyProfileController(vacancyResource, $stateParams) {
+module.exports = function VacancyProfileController(vacancyResource, $stateParams, imageService) {
   var vm = this;
 
   vm.subscribe = subscribe;
+  vm.getUserImageUrl = getUserImageUrl;
 
   vacancyResource.oneVacancy($stateParams.id).then(function(vacancy) {
     vm.vacancy = vacancy;
   });
+
+  vacancyResource.subscriptions($stateParams.id).then(subscriptions => vm.subscriptions = subscriptions);
 
   function subscribe(vacancy) {
     vacancyResource.subscribe(vacancy).then(function() {
       console.log('success subscruption!');
     });
   }
+
+  function getUserImageUrl(user) {
+    return imageService.getImageUrl(user, 'assets/images/user-default.png');
+  }
+
 };
