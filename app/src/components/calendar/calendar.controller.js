6e5b6fb3813fada30fcb0b48f6ecb5e47d20a603
Aleh Atsman <aleh_atsman@epam.com>
2016-02-07 19:07:02 +0300

    enchantment - event title.

--- app/src/components/calendar/calendar.controller.js
+++ app/src/components/calendar/calendar.controller.js
@@ -1,20 +1,40 @@
-module.exports = function CalendarController(Vendor, interviewResource) {
+module.exports = function CalendarController(Vendor, interviewResource, $compile, $scope) {
   const {moment} = Vendor;
 
   const vm = this;
 
-  vm.events = [];
+  vm.eventSources = [[]];
 
   interviewResource.list().then((interviews) => {
     vm.eventSources[0] = interviews.map((interview) => {
       interview.start = moment(interview.date);
-      interview.end = moment(interview.date).add(1, 'h').toDate();
-      interview.title = 'fadsf';
+      const message = ['Interview with', interview.candidate.name, interview.candidate.namesurname];
+      interview.title = message.join(' ');
       return interview;
     });
   });
 
-  vm.eventSources = [vm.events];
+  /* alert on eventClick */
+  vm.alertOnEventClick = function( date, jsEvent, view) {
+    console.log(date.title + ' was clicked ');
+  };
+  /* alert on Drop */
+  vm.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view) {
+    console.log('Event Droped to make dayDelta ' + delta);
+  };
+  /* alert on Resize */
+  vm.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view) {
+    console.log('Event Resized to make dayDelta ' + delta);
+  };
+
+  /* Render Tooltip */
+  vm.eventRender = function(event, element, view) {
+    element.attr({
+      'uib-tooltip': event.title,
+      'tooltip-placement': 'top'
+    });
+    $compile(element)($scope);
+  };
 
   vm.uiConfig = {
     calendar: {
@@ -29,7 +49,8 @@ module.exports = function CalendarController(Vendor, interviewResource) {
       columnFormat: 'dddd',
       dayClick: vm.alertEventOnClick,
       eventDrop: vm.alertOnDrop,
-      eventResize: vm.alertOnResize
+      eventResize: vm.alertOnResize,
+      eventRender: vm.eventRender
     }
   };
 };
