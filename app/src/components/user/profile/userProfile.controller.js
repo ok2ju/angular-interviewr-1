d406997b9679f59a8dd0b9798e7145305145ba8c
Ab0lt <andrei.baltrukevich@instinctools.ru>
2016-03-14 16:43:51 +0300

    added age of user

--- app/src/components/user/profile/userProfile.controller.js
+++ app/src/components/user/profile/userProfile.controller.js
@@ -1,10 +1,14 @@
-module.exports = function UserProfileController(userResource, config, $stateParams, imageService) {
+module.exports = function UserProfileController(userResource, config, $stateParams, imageService, Vendor) {
   const vm = this;
+  const {moment} = Vendor;
 
+  vm.user = {};
   vm.getImageUrl = getImageUrl;
 
   userResource.one($stateParams.id).then(function(user) {
     vm.user = user;
+    vm.user.age = moment().diff(moment(vm.user.dob, 'YYYYMMDD'), 'years');
+
   });
 
   function getImageUrl() {
