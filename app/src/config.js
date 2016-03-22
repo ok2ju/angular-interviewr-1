26c24af5d4776e0c279df681439aebc892b0c209
Aleh Atsman <olegruno@gmail.com>
2016-03-22 22:56:44 +0300

    contants changed

--- app/src/config.js
+++ app/src/config.js
@@ -4,7 +4,7 @@ angular.module('app.config', [])
   .constant('config', {
     'API_URL': 'http://localhost:3000',
     'ROOT_DIR': 'app',
-    'CONF_URL': 'https://localhost:3000/interview',
+    'CONF_URL': 'https://192.168.1.103:8123/interviewroom',
     states: {
       CALENDAR: 'app.calendar',
       CANDIDATES_GRID: 'app.candidates.grid'
